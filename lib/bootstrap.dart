6c23f95f08f6e5506555817643ee463e34583e72
João Baroni <jvitorbaroni@gmail.com>
2023-03-23 00:12:12 -0300

    feat: added crashanalytics

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,21 +1,45 @@
 import 'dart:async';
+import 'dart:isolate';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'firebase_options.dart';
 import 'main.dart';
 
 Future<void> bootstrap() async {
+  await _configureErrorsOutsideOfFlutter();
   await runZonedGuarded(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
+      await _configureFirebase();
       SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
       await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
       runApp(ModularApp(module: AppModule(), child: const AppWidget()));
     },
-    (error, stack) {
-      print(error);
+    (error, stack) async {
+      await FirebaseCrashlytics.instance.recordError(error, stack);
     },
+  );
+}
+
+Future<void> _configureFirebase() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+}
+
+Future<void> _configureErrorsOutsideOfFlutter() async {
+  Isolate.current.addErrorListener(
+    RawReceivePort((pair) async {
+      final List<dynamic> errorAndStacktrace = pair;
+      await FirebaseCrashlytics.instance.recordError(
+        errorAndStacktrace.first,
+        errorAndStacktrace.last,
+      );
+    }).sendPort,
   );
 }
